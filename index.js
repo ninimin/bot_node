53651df3e45976a06b0d1622266b0b95810255ee
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-10-19 17:06:03 +0700

    a

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const server = require('express');
-const PORT = process.env.PORT || 9999;
-const request = require('request');
-const bodyParser = require('body-parser');
-
 server()
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false}))
     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+    // เพิ่มส่วนของ Webhook เข้าไป
+    .post('/webhook', function (req, res) {
+        let replyToken = req.body.events[0].replyToken;
+        let msg = req.body.events[0].message.text;
+        
+        console.log(`Message token : ${ replyToken }`);
+        console.log(`Message from chat : ${ msg }`);
+
+        res.json({
+            status: 200,
+            message: `Webhook is working!`
+        });
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-// const http = require('httpa');
+////////////////////////////////////////////////////////
+// const server = require('express');
+// const PORT = process.env.PORT || 9999;
+// const request = require('request');
+// const bodyParser = require('body-parser');
+
+// server()
+//     .use(bodyParser.json())
+//     .use(bodyParser.urlencoded({ extended: false}))
+//     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+//     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+//////////////////////////////////////////////////////////////////////////////
+// const http = require('https');
 
 // const hostname = '127.0.0.1';
 // const port = 3000;
