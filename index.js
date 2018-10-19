9433b318202bad9298700d93fb8e4c615c06b20d
kanyamat <31055915+kanyamat@users.noreply.github.com>
2018-10-19 15:16:33 +0700

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * @author Pamontep Panya
- * @email pamontep.p@gmail.com
- * @create date 2018-06-03 03:45:02
- * @modify date 2018-06-03 03:45:02
- * @desc A sample project of Node.js and Line API
-*/
 const server = require('express');
 const PORT = process.env.PORT || 9999;
 const request = require('request');
